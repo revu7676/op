33e783628c5fb60c3c40f3e7d949bed4bd4eb6a0
revu7676 <revathisavant1@gmail.com>
2025-05-06 16:28:12 +0530

    Update BasicOperations.java

--- BasicOperations.java
+++ BasicOperations.java
@@ -1,7 +1,7 @@
 
 
 public class BasicOperations {
-    public static void main(String []) args){
+    public static void main(String [] args){
         int a = 15;
         int b = 0;
         int sum = a + b;

4b37f8d9eb86f33b0d2b1d8d91e4b38b794db35e
ashfiyakapali <ashfiyakapali@gmail.com>
2025-05-06 16:15:04 +0530

    main

---
+++ BasicOperations.java
@@ -0,0 +1,16 @@
+
+
+public class BasicOperations {
+    public static void main(String []) args){
+        int a = 15;
+        int b = 0;
+        int sum = a + b;
+        int diff = a - b;
+        int prod = a * b;
+    
+        System.out.println("Addition: " + sum);
+        System.out.println("Substraction: " + diff);
+        System.out.println("Division: " + prod);
+    
+        }
+}
